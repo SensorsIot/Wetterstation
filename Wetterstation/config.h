fcfd936fafa558fddfa082af65c80e73ff8d0811
SensorsIOT <andreas.spiess@arumba.com>
2025-11-27 20:57:22 +0100

    Initial commit

---
+++ Wetterstation/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// --- Konfiguration ---
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3D 
+#define BME_ADDRESS 0x76    
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+#endif
